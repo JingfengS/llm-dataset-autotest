c1e41a4c4b8284711a1bad0cd8058c32da6338f9
JingfengS <jingfeng050106@gmail.com>
2025-03-31 17:20:46 +0800

    Write direct scripts to evaluate llms

---
+++ src/internvl/scripts/evaluate_mme.py
@@ -0,0 +1,19 @@
+from models.BaseModelTest import ModelTestConfig, BaseModelTest
+from models.MME_TestModel import MME_TestModel
+import json
+from datasets import load_dataset, get_dataset_config_names
+import pandas as pd
+from pathlib import Path
+from tqdm import tqdm
+import os
+
+os.environ['OPENAI_API_KEY'] = 'jingfeng'
+os.environ['DEEPEVAL_RESULTS_FOLDER'] = '../mme_results'
+MME_MODEL_CONFIG = ModelTestConfig(model_name='openai/internvl2_5')
+
+if __name__ == '__main__':
+    mme_goldens_path = Path('../mme/goldens.pkl')
+    mme_model = MME_TestModel(MME_MODEL_CONFIG)
+    mme_model.load_goldens(mme_goldens_path)
+    mme_model.set_eval()
+    mme_model.evaluate_llm()
